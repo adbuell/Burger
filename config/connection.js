d2cce1bb6f3b0b30c1acfc63174cde5f8173953f
April Buell <adb701@gmail.com>
2018-03-13 20:28:52 -0600

    final

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+var mysql = require('mysql');
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'homework',
+        password: 'homework',
+        database: 'burgers_db'
+    });
+}
+
+connection.connect(function(error) {
+    if (error) {
+        console.error('error connecting: ' + error.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- work on the burgers_db
+USE `burgers_db`;
+
+-- insert 3 burgers
+INSERT INTO burgers (burger_name) VALUES ('Hamburger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Double Meat');
